4a119755cad1bfa517c72b1f1afea83cd33d5f01
Hussnain Nawaz <66999900+Hussnain-Nawaz@users.noreply.github.com>
2023-06-06 16:55:04 +0500

    Initial-Commit

---
+++ Assets/Scripts/HealthItem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthItem : MonoBehaviour
+{
+    [SerializeField] private string _tagToCompare = "Player";
+    [SerializeField] private int _healingPower = 10;
+    [SerializeField] private UnityEvent OnHeal;
+    private void OnTriggerEnter(Collider other) {
+        if (other.TryGetComponent(out Health health)) {
+            health.GainHealth(_healingPower);
+            OnHeal?.Invoke();
+        }
+    }
+}
